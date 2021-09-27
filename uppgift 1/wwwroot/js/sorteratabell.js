f30b206a7ffe0f68a0e0f62a4dc8f36e9daf1b08
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-09-27 15:47:42 +0200

    sortering i stigande ordning efter namn eller bostadsort fungerar

--- uppgift 1/wwwroot/js/sorteratabell.js
+++ uppgift 1/wwwroot/js/sorteratabell.js
@@ -1,5 +1,5 @@
+// direkt från Sedgewicks Algorithms: bubble-sort
 //
-// bubble-sort
 // void bubble(itemType a[], int N) {
 //   int i, j;
 //   for ( i = N ; i >= 1 ; i--)
@@ -8,52 +8,64 @@
 //           swap(a, j-1, j);
 //}
 //      efter en växling kommer nästa steg i den inre iterationen återigen att
-//      att jämföra samma ord.  Växlingen av just det ordet avbryts när det som kommer efter
-//      är mindre. OM detta ord är större än det som är till höger om det så börjar processen om igen med just det ordet
-//    SÅ:
-//   start: Ulf Bengt Micke Wei Simon
-//  i j
-//        Ulf Bengt Micke Wei Simon
-//  5 2         ^                     Ulf o Bengt växlar
-//        Bengt Ulf Micke Wei Simon
-//  5 3               ^               Ulf o Micke växlar
-//        Bengt Micke Ulf Wei Simon
-//  5 4                    ^          Ingen ändring
-//        Bengt Micke Ulf Wei Simon
-//  5 5                         ^     Wei o Simon växlar
-//  5 6                               j > i - yttre iteration
-//        Bengt Micke Ulf Simon Wei
-//  4 2           ^                    Ingen ändring
-//        Bengt Micke Ulf Simon Wei
-//  4 3               ^                Ingen ändring
-//        Bengt Micke Ulf Simon Wei
-//  4 4                     ^           Ulf o Simon växlar
-//  4 5                               j > i - yttre iteration
-//        Bengt Micke Simon Ulf Wei
-//  3 2             ^      Ingen ändring
-//        Bengt Micke Simon Ulf Wei
-//  3 3             ^      Ingen ändring
-//        Bengt Micke Simon Ulf Wei
-//  3 4                               j > i - yttre iteration
-//  2 2             ^      Ingen ändring
+//      att jämföra samma ord med det till höger.  Växlingen av just det ordet avbryts när det som kommer efter
+//      är större (i ordningen)
+//      det ordet som visade sig vara större kommer ordet i stället att få avancera i listan.
 //
-//  1 2
+// SÅ:
+// start: Ulf Bengt Micke Wei Simon
+// i j   0     1     2     3     4
+//       Ulf   Bengt Micke Wei   Simon
+// 5 1         ^                       Ulf o Bengt växlar
+//       Bengt Ulf   Micke Wei   Simon
+// 5 2               ^                 Ulf o Micke växlar
+//       Bengt Micke Ulf   Wei   Simon
+// 5 3                     ^           Ingen ändring men vi kan ha ett ord som är större ?
+//       Bengt Micke Ulf   Wei   Simon
+// 5 4                           ^     Wei o Simon växlar - ja det var större
+//       Bengt Micke Ulf   Simon Wei
 //
-//  0 -                    slut på yttre iteration
+// 5 5   slut på inre loop och ett ord mindre att bry sig om - dekrement för antalet aktuella ord
+//
+//       Bengt Micke Ulf   Simon Wei
+// 4 1         ^                       Ingen ändring
+//       Bengt Micke Ulf   Simon Wei
+// 4 2               ^                 Ingen ändring
+//       Bengt Micke Ulf   Simon Wei
+// 4 3                      ^          Ulf o Simon växlar
+//       Bengt Micke Simon Ulf   Wei
+//
+// 4 4   slut på inre loop
+//
+//       Bengt Micke Ulf   Simon Wei
+// 3 1         ^                       Ingen ändring
+//       Bengt Micke Ulf   Simon Wei
+// 3 2               ^                 Ingen ändring
+//       Bengt Micke Ulf   Simon Wei
+//
+// 3.3   slut på inre loop
+//
+//       Bengt Micke Ulf   Simon Wei
+// 2 1         ^                       Ingen ändring
+//       Bengt Micke Ulf   Simon Wei
+//
+// 2.2   slut på inre loop
+//
+//       Bengt Micke Ulf   Simon Wei
+//
+// 1 1   slut på inre loop
 
-function sorteraTabellStigande( sorteringskriteria)
+function sorteraTabellStigande( sorteringsterm)
 {
     var tabellen = document.getElementById( "personindex");
     var posterna = tabellen.rows;   // varje rad i tabellen
-    var i;   // antalet poster
-    var j;
+    var i;    // antalet poster
     var x, y;   // de två posterna som det ska beslutas om
 
-    for ( i = posterna.length; i >= 1 ; i-- ) {
-	j=1;
-	for ( j = 1; j < i ; j++) {
-	    x = posterna[j-1].getElementsByTagName("TD")[sorteringskriteria];
-	    y = posterna[j].getElementsByTagName("TD")[sorteringskriteria];
+    for ( i = posterna.length ; i >= 1 ; i-- ) {
+	for ( let j = 1; j < i ; j++) {
+	    x = posterna[j-1].getElementsByTagName("TD")[sorteringsterm];
+	    y = posterna[j].getElementsByTagName("TD")[sorteringsterm];
 
 	    if ( x!=undefined && y != undefined)
 		if ( x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() )
@@ -63,74 +75,34 @@ function sorteraTabellStigande( sorteringskriteria)
 }
 
 //
-// samma från början men man börjar i andra ändan
+// samma från början men man börjar i andra ändan (sort-of)
 //
-// function sorteraTabellSjunkande( sorteringskriteria) {
-//     var tabellen = document.getElementById( "personindex");
-//     var posterna = tabellen.rows;   // varje rad i tabellen
-//     var i = posterna.length;
-//     var j;
-//     var x, y;   // de två posterna som det ska beslutas om
+function sorteraTabellSjunkande( sorteringsterm) {
+    var tabellen = document.getElementById( "personindex");
+    var posterna = tabellen.rows;   // varje rad i tabellen
+    var i = posterna.length;
+    var j;
+    var x, y;   // de två posterna som det ska beslutas om
 
-//     for ( ; i >= 1 ; i++ )
-//	for ( j = 2; j <= i ; j--) {
-//	    x = posterna[i].getElementsByTagName("TD")[sorteringskriteria];
-//	    y = posterna[i+1].getElementsByTagName("TD")[sorteringskriteria];
+    for ( i = posterna.length ; i >= 1 ; i++ )
+	for ( j = 2; j <= i ; j--) {
+	    x = posterna[j-i].getElementsByTagName("TD")[sorteringsterm];
+	    y = posterna[j].getElementsByTagName("TD")[sorteringsterm];
 
-//	    if () {
-//		posterna[i].parentNode.insertBefore( posterna[i+1], posterna[i]);
-//	    }
-//	}
-// }
-
-
-function sorteraTabell( sorteringskriteria) {
-    alert('sorteraTabell');
-
-    var sorteringsordning = "stigande";  // sorteringsordning
-
-    // if ( sorteringsordning == "stigande")
-	sorteraTabellStigande( sorteringskriteria);
-    // else if ( sorteringsordning == "sjunkande")
-    //	sorteraTabellSjunkande( sorteringskriteria);
+	    // if () {
+	    //	posterna[i].parentNode.insertBefore( posterna[i+1], posterna[i]);
+	    // }
+	}
 }
 
-// function sorteraTabell( sorteringskriteria) {
-//     alert('sorteraTabell');
+//
+// begränsad - den sorterar enbart på namn eller bostadsort (sorteringsterm) i stigande ordning
+//
+function sorteraTabell( sorteringsterm) {
+    var sorteringsordning = "stigande";  // sorteringsordning
 
-//     // var tabellen = document.getElementById( "personindex"),
-//     var tabellen = document.getElementById( "personindex");
-//     var sorteringsordning = "stigande";  // sorteringsordning
-//     var posterna;                   // varje rad i tabellen
-//     var antalOmflyttningar = 0;     // hur många omkastningar
-//     let i=0;                        // itereringsvariabel
-//     var x, y;
-
-//	posterna = document.getElementById( "personindex").rows;
-
-
-//	i = 2;
-//	while (( i < (posterna.length )) && ! felaktigsortering) {
-//	    x = posterna[i].getElementsByTagName("TD")[sorteringskriteria];
-//	    y = posterna[i+1].getElementsByTagName("TD")[sorteringskriteria];
-
-//	    switch( sorteringsordning) {
-//	    case "stigande":
-//		if ( x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//		    felaktigsortering = true;
-//		}
-//		break;
-//		case "sjunkande":
-//		if ( x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-//		    felaktigsortering = true;
-//		}
-//		break;
-//	    }
-//	    i++;
-//	}
-//	if ( felaktigsortering) {
-//	    posterna[i].parentNode.insertBefore( posterna[i+1], posterna[i]);
-//	    antalOmflyttningar++;
-//	    omkastninggjord=true;
-//	}
-//     }
+    if ( sorteringsordning == "stigande")
+	sorteraTabellStigande( sorteringsterm);
+    // else if ( sorteringsordning == "sjunkande")
+    //	sorteraTabellSjunkande( sorteringsterm);
+}
