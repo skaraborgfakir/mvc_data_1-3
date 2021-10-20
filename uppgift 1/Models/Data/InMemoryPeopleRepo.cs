a27782ef4644dfed6c896971b9e5e81080e569ee
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-21 01:14:59 +0200

    LINQ fråga i  InMemoryRepo

--- uppgift 1/Models/Data/InMemoryPeopleRepo.cs
+++ uppgift 1/Models/Data/InMemoryPeopleRepo.cs
@@ -77,13 +77,7 @@ namespace Kartotek.Modeller.Data {
 	// används från PeopleService
 	//
 	public Person Read( int id) {
-			return kartoteket.Find(person => person.Id == id);
-	 //   foreach ( Person item in kartoteket) {
-		//if ( item.Id == id)
-		//{
-		//    return item;
-		//}
-	 //   }
+			return kartoteket.FirstOrDefault(person => person.Id == id);				 
 
 	    // borde vara omöjligt att komma hit
 	    throw new ArgumentException( "felaktigt id i InMemoryPeopleRepo:Read");
