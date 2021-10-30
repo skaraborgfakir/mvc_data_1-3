5ae15bd78deb61b71a52fc8149cb5b5e64695750
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-30 22:27:41 +0200

    bygger ut ajax-kontrollanten

--- uppgift 1/Controllers/PeopleAjaxController.cs
+++ uppgift 1/Controllers/PeopleAjaxController.cs
@@ -1,3 +1,9 @@
+//
+// Time-stamp: <2021-10-30 19:44:38 stefan>
+//
+// ett personkartotek
+//
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,56 +19,59 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Serialization;
 
 using Kartotek.Modeller;
+using Kartotek.Modeller.Interfaces;
 using Kartotek.Modeller.Entiteter;
 using Kartotek.Modeller.Vyer;
 
 namespace Kartotek.Controllers {
-   // [Route( "api/[controller]" )]
-   //[ApiController]
-   public class PeopleAjaxController : Controller {
-      private readonly ILogger<PeopleController> _loggdest;
-      private readonly IConfiguration _configurationsrc;
-      private readonly IWebHostEnvironment _webHostEnvironment;
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PeopleAjaxController : ControllerBase {
+	private readonly ILogger<PeopleAjaxController> _loggdest;
+	private readonly IConfiguration _configurationsrc;
+	private readonly IWebHostEnvironment _webHostEnvironment;
 
-      public PeopleAjaxController ( ILogger<PeopleController> loggdest,
-                            IConfiguration configurationsrc,
-                           IWebHostEnvironment webHostEnvironment ) {
-         _loggdest = loggdest;
-         _configurationsrc = configurationsrc;
-         _webHostEnvironment = webHostEnvironment;
-      }
+	//
+	// kontroller-klassen knyter ihop affärs-/process-logik (serviceenhten) med UI
+	// kontroller implementeras utgående från serviceenhetens termer
+	//
+	private readonly IPeopleService _serviceenheten;
 
-      //
-      // funktioner/aktioner i labb 3 - AJAX
-      //
-      [HttpGet( "listan" )]
-      [ActionName( "uppdateralistan" )]
-      [Produces( "application/json" )]
-      public JsonResult uppdateraListanUrDatabasen () {
-         _loggdest.LogInformation( string.Concat( "public JsonResult uppdateraListanUrDatabasen " ) );
+	public PeopleAjaxController ( ILogger<PeopleAjaxController> loggdest,
+				      IConfiguration configurationsrc,
+				      IWebHostEnvironment webHostEnvironment,
+				      IPeopleService serviceenheten ) {
+	    _loggdest = loggdest;
+	    _configurationsrc = configurationsrc;
+	    _webHostEnvironment = webHostEnvironment;
+	    _serviceenheten = serviceenheten;
+	}
 
-         string filePath = _webHostEnvironment.ContentRootPath + "/App_Data/test.json";
+	//
+	// funktioner/aktioner i labb 3 - AJAX
+	//
+	[ActionName( "uppdateralistan" )]  // API mot JS !!
+	public ActionResult uppdateraListanUrDatabasen () {
+	    _loggdest.LogInformation( (new System.Diagnostics.StackFrame(0, true).GetMethod()) + " rad : " + (new System.Diagnostics.StackFrame(0, true).GetFileLineNumber().ToString()));
 
-         throw new NotImplementedException( "public JsonResult tagFramKortet())" );
-         // return System.IO.File.ReadAllText( filePath );
-      }
+	    return Ok( _serviceenheten.Utdraget());
+	}
 
-      [HttpGet( "{id}" )]
-      [ActionName( "tagUppKortet" )]
-      [Produces( "application/json" )]
-      public JsonResult tagFramKortet ( int id ) {
-         _loggdest.LogInformation( string.Concat( "public JsonResult tagFramKortet " ) );
+	[HttpGet( "{id=1}" )]
+	[ActionName( "tagUppKortet" )]  // API mot JS !!
+	[Produces( "application/json" )]
+	public ActionResult tagFramKortet ( int id ) {
+	    _loggdest.LogInformation( (new System.Diagnostics.StackFrame(0, true).GetMethod()) + " rad : " + (new System.Diagnostics.StackFrame(0, true).GetFileLineNumber().ToString()));
 
-         throw new NotImplementedException( "public JsonResult tagFramKortet())" );
-      }
+	    return Ok( _serviceenheten.FindBy( id));
+	}
 
-      [HttpDelete( "{id}" )]
-      [ActionName( "rensaBortKortet" )]
-      public IActionResult rensaUrKortet ( int id ) {
-         _loggdest.LogInformation( string.Concat( "public IActionResult rensaUrKortet " ) );
+	[HttpDelete( "{id}" )]
+	[ActionName( "kaseraKortet" )]  // API mot JS !!
+	public ActionResult rensaUrKortet ( int id ) {
+	    _loggdest.LogInformation( (new System.Diagnostics.StackFrame(0, true).GetMethod()) + " rad : " + (new System.Diagnostics.StackFrame(0, true).GetFileLineNumber().ToString()));
 
-         throw new NotImplementedException( "public IActionResult rensaUrKortet())" );
-      }
-
-   }
+	    throw new NotImplementedException( "public IActionResult rensaUrKortet())" );
+	}
+    }
 }
