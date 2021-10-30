a69704b801ea0a27f4abedf037d9e67357da880c
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-30 21:39:15 +0200

    modifierar interface i people
    ny funktion för uthämtning av en lista av kort, ej i vymodell

--- uppgift 1/Models/Interfaces/IPeopleService.cs
+++ uppgift 1/Models/Interfaces/IPeopleService.cs
@@ -1,20 +1,29 @@
 //
-// Time-stamp: <2021-09-16 14:33:15 stefan>
+// Time-stamp: <2021-10-30 21:37:44 stefan>
 //
 // ett åtagande för ett gränssnitt till ett personkartotek
 //
+
+using System.Collections.Generic;
 
 using Kartotek.Modeller.Entiteter;
 using Kartotek.Modeller.Vyer;
 
 namespace Kartotek.Modeller.Interfaces {
-   public interface IPeopleService {
-      Person Add ( CreatePersonViewModell person );        // inläggning av ett kort
-                                                           // använder Create i PeopleRepo
-      PeopleViewModell All ();                            // totalutdrag från registret
-      PeopleViewModell FindBy ( PeopleViewModell search ); // specifik(a) kort
-      Person FindBy ( int id );                            // ett specifikt kort
-      Person Edit ( int id, Person person );               // modifiering av ett kort med ett visst nummer
-      bool Remove ( int id );                              // borttagning av ett visst kort
-   }
+    public interface IPeopleService {
+	//
+	// använder enbart entiteter direkt
+	//
+	List<Person> Utdraget ();              // totalutdrag från registret, en ren lista
+	Person FindBy ( int id );              // ett specifikt kort
+	Person Edit ( int id, Person person ); // modifiering av ett kort med ett visst nummer
+	bool Remove ( int id );                // borttagning av ett visst kort
+
+	//
+	// använder vymodellerna
+	//
+	PeopleViewModell All ();                             // totalutdrag från registret, som vymodell
+	PeopleViewModell FindBy ( PeopleViewModell search ); // specifik(a) kort, som vymodell
+	Person Add ( CreatePersonViewModell person );        // inläggning av ett kort, använder Create i PeopleRep
+    }
 }
