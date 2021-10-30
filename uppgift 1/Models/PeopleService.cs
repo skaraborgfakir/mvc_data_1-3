7ec594a7a49644947d066f2466ba0fd63ab1adae
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-30 22:14:58 +0200

    omarrangenmang av koden - placera de funktioner som enbart använder
    entiteter eller ID i början, de som utgår från vymodeller längre ner
    
    XML:uppmärkning av funktioner och metoder som sedan kan användas av
    intellivision eller för att via exv doxygen och LaTeX generera kod-dokumentation

--- uppgift 1/Models/PeopleService.cs
+++ uppgift 1/Models/PeopleService.cs
@@ -1,4 +1,4 @@
-// Time-stamp: <2021-09-20 16:16:40 stefan>
+// Time-stamp: <2021-10-30 22:14:30 stefan>
 //
 
 using System;
@@ -11,106 +11,127 @@ using Kartotek.Modeller.Entiteter;
 using Kartotek.Modeller.Vyer;
 
 namespace Kartotek.Modeller {
-   public class PeopleService : IPeopleService {
-      private static InMemoryPeopleRepo kartoteket = new InMemoryPeopleRepo();
+    public class PeopleService : IPeopleService {
+	private static InMemoryPeopleRepo kartoteket = new InMemoryPeopleRepo();
 
-      public Person Add ( CreatePersonViewModell nyttKort ) {
-         return kartoteket.Create( namn: nyttKort.Namn,
-                    bostadsort: nyttKort.Bostadsort,
-                    telefonnummer: nyttKort.Telefonnummer );
-      }
+	/// <summary>
+	/// gör ett komplett utdrag ur registret
+	/// enbart i form av entiteter, ej vymodell
+	/// </summary>
+	/// <returns>
+	/// lista av personer
+	/// </returns>
+	/// <see cref="PeopleAjaxController"/>
+	public List<Person> Utdraget() {
+	    return kartoteket.Read();
+	}
 
-      //
-      // sökning efter poster
-      //
-      // kriterier : inga
-      //
-      public PeopleViewModell All () {
-         PeopleViewModell vyn = new PeopleViewModell();
-         vyn.Utdraget = kartoteket.Read();
+	/// <summary>
+	/// plocka fram ett visst kort
+	/// enbart i form av entiteter, ej vymodell
+	/// </summary>
+	public Person FindBy ( int id ) {
+	    return kartoteket.Read( id: id );
+	}
 
-         return vyn;
-      }
+	/// <summary>
+	/// modifiering av ett specifikt kort
+	/// enbart i form av entiteter, ej vymodell
+	/// </summary>
+	public Person Edit ( int id, Person person ) {
+	    // Not developed yet.
+	    throw new NotImplementedException();
+	}
 
-      //
-      // kriterier : namn
-      //
-      private PeopleViewModell FindByNamn ( String namn,
-                       List<Person> poster,
-                       PeopleViewModell vyn ) {
-         Console.WriteLine( "public PeopleViewModell FindByNamn( String namn," );
-         foreach (Person item in poster) {
-            if (namn.Equals( item.Namn, StringComparison.OrdinalIgnoreCase )) {
-               vyn.Utdraget.Add( item );
-            }
-         }
+	/// <summary>
+	/// kasering av ett specifikt kort
+	/// enbart i form av entiteter, ej vymodell
+	/// </summary>
+	public bool Remove ( int id ) {
+	    Person kortet = this.FindBy( id );
 
-         return vyn;
-      }
-      //
-      // kriterier : bostadsort
-      //
-      private PeopleViewModell FindByBostadsort ( String bostadsort,
-                        List<Person> poster,
-                        PeopleViewModell vyn ) {
-         Console.WriteLine( "public PeopleViewModell FindByBostadsort( String bostadsort" );
+	    if (kortet != null)
+		return kartoteket.Delete( kortet );
 
-         foreach (Person item in poster) {
-            if (bostadsort.Equals( item.Bostadsort, StringComparison.OrdinalIgnoreCase )) {
-               vyn.Utdraget.Add( item );
-            }
-         }
+	    return false;
+	}
 
-         return vyn;
-      }
+	/// <summary>
+	/// tillägg av ett kort
+	/// utgår från en vymodell
+	/// </summary>
+	public Person Add ( CreatePersonViewModell nyttKort ) {
+	    return kartoteket.Create( namn: nyttKort.Namn,
+				      bostadsort: nyttKort.Bostadsort,
+				      telefonnummer: nyttKort.Telefonnummer );
+	}
 
-      //
-      // sökning efter poster
-      //
-      // kriterier :
-      //    namn
-      //  eller
-      //    bostadsort
-      //
-      public PeopleViewModell FindBy ( PeopleViewModell sökterm ) {
-         List<Person> poster = kartoteket.Read();
-         PeopleViewModell vyn = new PeopleViewModell();
-         vyn.Utdraget = new List<Person>();
+	/// <summary>
+	/// sökning efter poster - utgår från en vymodell som sökterm
+	/// kriterier : inga, dvs alla kort ska hämtas och levereras
+	/// </summary>
+	public PeopleViewModell All () {
+	    PeopleViewModell vyn = new PeopleViewModell() { Utdraget = kartoteket.Read() };
 
-         if (!String.IsNullOrEmpty( sökterm.Namn )) {
-            Console.WriteLine( "public PeopleViewModell FindBy( PeopleViewModell namn" );
-            vyn = FindByNamn( namn: sökterm.Namn,
-                    poster: poster,
-                    vyn: vyn );
-         } else if (!String.IsNullOrEmpty( sökterm.Bostadsort )) {
-            Console.WriteLine( "public PeopleViewModell FindBy( PeopleViewModell bostadsort" );
-            vyn = FindByBostadsort( bostadsort: sökterm.Bostadsort,
-                     poster: poster,
-                     vyn: vyn );
-         }
+	    return vyn;
+	}
 
-         return vyn;
-      }
+	/// <summary>
+	/// sökning efter poster - utgår från en vymodell som sökterm
+	/// kriterier : namn
+	/// </summary>
+	private PeopleViewModell FindByNamn ( String namn,
+					      List<Person> poster,
+					      PeopleViewModell vyn ) {
+	    foreach (Person item in poster) {
+		if (namn.Equals( item.Namn, StringComparison.OrdinalIgnoreCase )) {
+		    vyn.Utdraget.Add( item );
+		}
+	    }
 
-      public Person FindBy ( int id ) {
-         return kartoteket.Read( id: id );
-      }
+	    return vyn;
+	}
 
-      public Person Edit ( int id, Person person ) {
-         // Not developed yet.
-         throw new NotImplementedException();
-      }
+	/// <summary>
+	/// sökning efter poster - utgår från en vymodell som sökterm
+	/// kriterier : bostadsort
+	/// </summary>
+	private PeopleViewModell FindByBostadsort ( String bostadsort,
+						    List<Person> poster,
+						    PeopleViewModell vyn ) {
+	    foreach (Person item in poster) {
+		if (bostadsort.Equals( item.Bostadsort, StringComparison.OrdinalIgnoreCase )) {
+		    vyn.Utdraget.Add( item );
+		}
+	    }
 
-      public bool Remove ( int id ) {
-         Person kortet = this.FindBy( id );
+	    return vyn;
+	}
 
-         if (kortet == null) {
-            return false;
-         } else {
-            return kartoteket.Delete( kortet );
-         }
-         // Not developed yet.
-         //throw new NotImplementedException();
-      }
-   }
+	/// <summary>
+	/// sökning efter poster - utgår från en vymodell som sökterm
+	/// kriterier :
+	///    namn
+	///  eller
+	///    bostadsort
+	/// </summary>
+	public PeopleViewModell FindBy ( PeopleViewModell sökterm ) {
+	    List<Person> poster = kartoteket.Read();
+	    PeopleViewModell vyn = new PeopleViewModell();
+	    vyn.Utdraget = new List<Person>();
+
+	    if (!String.IsNullOrEmpty( sökterm.Namn )) {
+		vyn = FindByNamn( namn: sökterm.Namn,
+				  poster: poster,
+				  vyn: vyn );
+	    } else if (!String.IsNullOrEmpty( sökterm.Bostadsort )) {
+		vyn = FindByBostadsort( bostadsort: sökterm.Bostadsort,
+					poster: poster,
+					vyn: vyn );
+	    }
+
+	    return vyn;
+	}
+
+    }
 }
